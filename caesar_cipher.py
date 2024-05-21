97a77a9178fff8833740777bf20c4b3a5aa2cd1c
Hampton T <166450619+htimm1986@users.noreply.github.com>
2024-05-21 11:23:24 -0400

    Add files via upload
    Fun take on the Caesar Cipher that allows you to encode and decode a message.

---
+++ caesar_cipher.py
@@ -0,0 +1,59 @@
+#####################################
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+#############################################
+
+############################################
+######Function
+def caesar(text, shift_amount, direction):
+    output = ""
+    for letter in text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            if direction == "encode":
+                new_position = shift_amount + position
+            elif direction == "decode":
+                new_position = position - shift_amount 
+            new_letter = alphabet[new_position]
+            output += new_letter
+        else:
+            output += letter
+    print(f"The {direction}d text is: {output}")
+####User Input Area, greeting, and loop.
+######################
+continue1 = True
+print(logo)
+while continue1 == True:
+    direction = input("Type 'encode' to encrypt, or type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesar(text=text, shift_amount=shift, direction=direction)
+    
+##########Continue program?#################  
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.")
+    if restart == "yes":
+        continue1 = True
+    elif restart == "no":
+        continue1 = False
+        print("Goodbye!")
+    
+
+
+
